1cbc074206d7386709d4d9a8e6e923a846cd6a13
deek <hekayaman@hotmail.de>
2020-09-23 23:36:29 +0200

    added configuration package with configuation class encapsulation service call

---
+++ PoliticalSpeeches/src/main/java/com/pcand/kayaman/client/config/EncapsulationConfig.java
@@ -0,0 +1,11 @@
+package com.pcand.kayaman.client.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.pcand.kayaman.service.IPoliticalSpeech;
+import com.pcand.kayaman.service.PoliticalSpeechService;
+
+@Configuration
+public class EncapsulationConfig {
+}
