550ff8189fc29edee945acaa4160986d43fa40ab
deek <hekayaman@hotmail.de>
2020-09-25 00:57:30 +0200

    added bean InternalResourceServer to autowire in PoliticalSpeechService

--- PoliticalSpeeches/src/main/java/com/pcand/kayaman/client/config/EncapsulationConfig.java
+++ PoliticalSpeeches/src/main/java/com/pcand/kayaman/client/config/EncapsulationConfig.java
@@ -1,13 +1,24 @@
 package com.pcand.kayaman.client.config;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
 
+import com.pcand.kayaman.http.config.InternalHttpServerConfig;
+import com.pcand.kayaman.http.server.InternalResourceServer;
 import com.pcand.kayaman.service.IPoliticalSpeech;
 import com.pcand.kayaman.service.PoliticalSpeechService;
 
 @Configuration
+@EnableAutoConfiguration
+@ContextConfiguration(classes = {InternalHttpServerConfig.class})
 public class EncapsulationConfig {
+	
+	@Bean
+	public InternalResourceServer internalResourceServer() {
+		return new InternalResourceServer();
+	}
 	
 	@Bean
 	public IPoliticalSpeech politicalSpeeches() {
